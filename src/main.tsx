44fa56b1ec68a04e220190469ce427d2131d54f0
guilherme <guilherme.lamim96@gmail.com>
2023-02-22 14:26:03 -0300

    adicionando analytics vercel

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Home from './pages/Home'
 import './index.css'
+import { inject } from '@vercel/analytics';
 
+inject();
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Home />
